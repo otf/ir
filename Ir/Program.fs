f61193f8f9a2b9568f8f2964e94abf0380c0d436
otf <phoenixsingleton@gmail.com>
2014-06-14 18:49:24 +0900

    refactor for run.

--- Ir/Program.fs
+++ Ir/Program.fs
@@ -12,6 +12,23 @@ let run (Kleisli f) = f () |> runIO
 let getLine = async { return System.Console.ReadLine() }
 let print x = async { printf "%O" x}
 
+let application env = function
+  | [| "recv" |] ->
+    Kleisli <| konst (env >>= (uncurry receive))
+    >>>> Kleisli (toJSON >> print)
+
+  | [| "send" |] ->
+    let send = fun msg -> ((uncurry send) <!> env) >>= ((|>) msg)
+    Kleisli (konst getLine)
+    >>>> Kleisli (JsonValue.Parse >> fromJSON >> result) 
+    >>>> (Kleisli print |||| Kleisli send) 
+
+  | [| "--version" |] ->
+    Kleisli <| konst (print "1.0.0.0")
+
+  | _ ->
+    Kleisli <| konst (print "help")
+
 [<EntryPoint>]
 let main argv = 
   let env = monad {
@@ -21,25 +38,6 @@ let main argv =
     return (http, device)
   }
 
-  match argv with
-  | [| "recv" |] ->
-    Kleisli <| konst (env >>= (uncurry receive))
-    >>>> Kleisli (toJSON >> print)
-    |> run
-
-  | [| "send" |] ->
-    let send = fun msg -> ((uncurry send) <!> env) >>= ((|>) msg)
-    Kleisli (konst getLine)
-    >>>> Kleisli (JsonValue.Parse >> fromJSON >> result) 
-    >>>> (Kleisli print |||| Kleisli send) 
-    |> run
-
-  | [| "--version" |] ->
-    print "1.0.0.0" 
-    |> runIO
-
-  | _ ->
-    print "help"
-    |> runIO
+  run (application env argv)
   
   0
