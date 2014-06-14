10d19b4cc7d7c598b2812a92b406070c34177ef0
otf <phoenixsingleton@gmail.com>
2014-06-14 14:09:11 +0900

    add send command.
    add send command.
    
    refactor for env.

--- Ir/Program.fs
+++ Ir/Program.fs
@@ -1,9 +1,39 @@
-﻿[<EntryPoint>]
+﻿open FSharpPlus
+open System
+open System.Net.Http
+open System.Json
+open Fleece
+open IrKit
+
+let uncurry f = (<||) f
+
+[<EntryPoint>]
 let main argv = 
+  let env = async {
+    use http = new HttpClient()
+    let! devices = lookup zeroconfResolver
+    let device = devices |> List.head
+    return (http, device)
+  }
+
   match argv with
+  | [| "recv" |] ->
+    (uncurry receive) =<< env
+    |> Async.RunSynchronously
+    |> toJSON
+    |> printfn "%O"
+
+  | [| "send" |] ->
+    async {
+      let! env = env
+      let input = Console.ReadLine() |> JsonValue.Parse |> fromJSON
+      return! input |> choice (printf "%s" >> result) (env |> uncurry send)
+    }
+    |> Async.RunSynchronously
+
   | [| "--version" |] ->
     printf "1.0.0.0"
-    0
   | _ ->
     printf "help"
-    0
+  
+  0
